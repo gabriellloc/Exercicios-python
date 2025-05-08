cf5b1e399265c50e470a08de5ab56ea6aa50a71b
gabriellloc <gabrielocdev595@gmail.com>
2025-05-08 12:27:08 -0300

    Initial commit

---
+++ exercicios-gustavo/ex002.py
@@ -0,0 +1,8 @@
+nome = input("Qual é o seu nome? ")
+#print("Olá, é um prazer te connhecer,", nome+"!")
+
+print('Olá, é um prazer te conhecer, {}!'.format(nome))
+#outra forma de fazer:
+#print("Olá, é um prazer te conhecer, {usuario}!".format(usuario=nome))
+#outra forma de fazer:
+#print("Olá, é um prazer te conhecer, {nome}!".format(nome=nome))
