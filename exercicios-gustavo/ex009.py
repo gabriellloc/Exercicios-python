811e41e86e7171e7ec381657121a388dd191bce5
gabriellloc <gabrielocdev595@gmail.com>
2025-05-12 23:33:13 -0300

    Update ex009.py

--- exercicios-gustavo/ex009.py
+++ exercicios-gustavo/ex009.py
@@ -1,6 +1,6 @@
 num = int(input("Digite um valor para obter sua tabuada: "))
 
-print("--------")
+print("-------------")
 print(num,"x 1 =",num)
 print(num,"x 2 =",num*2)
 print(num,"x 3 =",num*3)
@@ -15,3 +15,4 @@ print("{num} x 8 = {res}".format(num=num, res=num*8))
 
 print("{} x 9 = {}". format(num, num*9))
 print("{} x 10 = {}". format(num, num*10))
+print("-------------")
