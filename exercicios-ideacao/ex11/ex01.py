f1d3932907b4d4c1971ff12b6c7ca0180d6eefb6
gabriellloc <gabrielocdev595@gmail.com>
2025-05-13 11:15:58 -0300

    Update ex01.py

--- exercicios-ideacao/ex11/ex01.py
+++ exercicios-ideacao/ex11/ex01.py
@@ -1,6 +1,5 @@
 num = int(input("Digite um valor: "))
 
-
 if num%10 == 0 and num%5 == 0 and num%2==0:
     print("Divisível por 10, 5 e 2")
 elif num%10 == 0:
@@ -9,3 +8,5 @@ elif num%5 == 0:
     print("Divisível por 5")
 elif num%2 == 0:
     print("Divisível por 2")
+else:
+    print("Não é divisível por 10, 5 e nem por 2")
