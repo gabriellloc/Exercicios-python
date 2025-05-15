dc3a1b173ba2a5d16678da21ec5ff43af171f45b
gabriellloc <gabrielocdev595@gmail.com>
2025-05-15 14:03:51 -0300

    Update ex011.py

---
+++ exercicios-gustavo/ex011.py
@@ -0,0 +1,5 @@
+lar = float(input("Digite o valor da largura: "))
+alt = float(input("Digite o valor da altura: "))
+
+print("A área é de {}m".format(lar*alt))
+print("A quantidade de tinta necessaria é de {}".format((lar*alt)/2))
