61113bdf1dc3a2203ab7542d2697e6e7fa9fb8c1
gabriellloc <gabrielocdev595@gmail.com>
2025-05-20 14:07:11 -0300

    Roleta Russa

--- caio..caio/ex01.py
+++ caio..caio/ex01.py
@@ -1,3 +1,13 @@
 import random
-num = random.randint(1,6)
-print(num)
+win = random.randint(1,6)
+
+num = int(input("Digite um valor entre 1 e 6: "))
+
+if num > 6 or num < 1:
+    print("Digite um valor entre 1 e 6")
+else:
+    if num == win:
+        print("Você acertou!")
+    else:
+        print("Você errou...")
+        print("O número era {}".format(win))
