6e0559e6b32cba14c2815fe57dbf9261e82bb65f
gabriellloc <gabrielocdev595@gmail.com>
2025-05-26 12:45:49 -0300

    tudo

--- caio..caio/ex02.py
+++ caio..caio/ex02.py
@@ -1,11 +1,18 @@
 prompt = "{0}: "
 print("Digite a idade de 10 pessoas")
-idade = int(input(prompt.format(1)))
+# idade = int(input(prompt.format(1)))
 num = 0
-for i in range(2, 4):
+for i in range(1, 11):
     idade = int(input(prompt.format(i)))
-    if idade == 18:
-        num + 1
+    if idade > 18 or idade == 18:
+        num += 1
         print(num)
 
 print("O número de pessoas com 18 anos é:", num)
+
+
+for i in range(1, 11):
+    idade = int(input(prompt.format(i)))
+    if idade > 18 or idade == 18:
+        num += 1
+        print(num)
