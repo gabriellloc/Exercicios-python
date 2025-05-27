87f8bee51b04f53c6c161189d5b1a5f0318cc08b
gabriellloc <gabrielocdev595@gmail.com>
2025-05-27 10:04:02 -0300

    Update ex04.py

--- exercicios-ideacao/ex12/ex04.py
+++ exercicios-ideacao/ex12/ex04.py
@@ -1,5 +1,12 @@
+par = 0
+impar = 0
+
 for i in range(1, 51):
     if (i % 2) ==0:
         print(i,"Par")
+        par += 1
     else:
         print(i,"Ímpar")
+        impar += 1
+
+print("Tem {} pares e {} impares".format(par, impar))
