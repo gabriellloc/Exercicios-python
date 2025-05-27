19a7405414c5a3fa8ac46ee6af003dca8574a4eb
gabriellloc <gabrielocdev595@gmail.com>
2025-05-27 09:35:15 -0300

    Create ex02.py

---
+++ exercicios-ideacao/ex12/ex02.py
@@ -0,0 +1,12 @@
+for i in range(10 , 21):
+    print("Tabuada do {}".format(i))
+    print(i * 1)
+    print(i * 2)
+    print(i * 3)
+    print(i * 4)
+    print(i * 5)
+    print(i * 6)
+    print(i * 7)
+    print(i * 8)
+    print(i * 9)
+    print(i * 10)
