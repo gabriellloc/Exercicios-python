a1afecd45047af633e3637b0ef980ec9b22bdd99
gabriellloc <gabrielocdev595@gmail.com>
2025-05-31 15:49:00 -0300

    ex08 ideacao

---
+++ exercicios-ideacao/ex12/ex08.py
@@ -0,0 +1,11 @@
+# for i in range(10):
+#     num = int(input("Digite um valor: "))
+#     print(num**2)
+
+#Dois modelos de responder
+
+num = []
+for i in range(10):
+    valor = int(input("Digite um valor: "))
+    num.append(valor**2)
+print(num)
