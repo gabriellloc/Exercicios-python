ebc512b3afb104489db77056ddc118f888bd49b2
gabriellloc <gabrielocdev595@gmail.com>
2025-05-31 18:20:46 -0300

    Create ex03.py

---
+++ exercicios-ideacao/ex11/ex03.py
@@ -0,0 +1,6 @@
+a1 = int(input("Digite o primeiro valor da sequência: "))
+r = int(input("Digite a razao: "))
+
+termo20 = a1 + ((20-1)*r)
+
+print("O vigésimo termo da sequência é: {}".format(termo20))
