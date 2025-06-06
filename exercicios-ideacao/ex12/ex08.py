be42551407c9044947dd26b5d9b47c4a0fc02fc2
gabriellloc <gabrielocdev595@gmail.com>
2025-06-06 00:10:46 -0300

    Update ex08.py

--- exercicios-ideacao/ex12/ex08.py
+++ exercicios-ideacao/ex12/ex08.py
@@ -1,4 +1,6 @@
-sequencia = ["primeira", "segunda"]
+import time
+
+sequencia = ["primeira", "segunda", "terceira", "quarta", "quinta"]
 
 cidade = []
 numero_veiculos = []
@@ -14,16 +16,19 @@ menor_indice = min(acidentes)
 
 for pos, indice in enumerate(acidentes):
     if indice == maior_indice:
+        time.sleep(1)
         print(f"O maior índice de acidentes é {maior_indice} e pertece a cidade {cidade[pos]}")
 
 
 soma = sum(numero_veiculos)
 div = len(numero_veiculos)
 media = soma/div
+time.sleep(1)
 print(f"A média de acidentes das cidades é igual a {media:.2f}")
 
 
 for posi_cid, cid in enumerate(cidade):
     if numero_veiculos[posi_cid] < 2000:
         media_veiculosAci = numero_veiculos[posi_cid]/acidentes[posi_cid]
+        time.sleep(1)
         print(f"A média de veiculos por acidentes é {media_veiculosAci:.2f} da cidade {cid}")
