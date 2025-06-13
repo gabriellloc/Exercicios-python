0ff38381473a83120b299f554d33e8bf336299e7
gabriellloc <gabrielocdev595@gmail.com>
2025-06-12 23:02:56 -0300

    Create ex09.py

---
+++ exercicios-ideacao/ex10/ex09.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+
+if n1%n2 == 0:
+    print(f"O primeiro valor é divisivel pelo segundo e o resultado é {n1/n2}")
+
+else:
+    print("O primeiro valor nao é divisivel pelo segundo")
