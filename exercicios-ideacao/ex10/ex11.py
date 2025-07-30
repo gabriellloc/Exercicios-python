1dcb29a89576173c90238742ebcfb2cf30f654a8
gabriellloc <gabrielocdev595@gmail.com>
2025-07-29 22:00:58 -0300

    Create ex11.py

---
+++ exercicios-ideacao/ex10/ex11.py
@@ -0,0 +1,19 @@
+#n1 = int(input('Digite o primeiro valor: '))
+#n2 = int(input('Digite o segundo valor: '))
+
+#print(max(n1, n2))
+#print(min(n1, n2))
+
+
+#para varios numeros:
+
+
+
+n10 = int(input('Digite o primeiro valor: '))
+n20 = int(input('Digite o segundo valor: '))
+n30 = int(input('Digite o terceiro valor: '))
+n40 = int(input('Digite o quarto valor: '))
+lista = [n10,n20,n30,n40]
+lista.sort()
+
+print(lista)
